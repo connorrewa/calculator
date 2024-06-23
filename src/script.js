e1e68e6e71bb5569423c1d6599e939d34c14e5db
connorrewa <connorrewa13@hotmail.com>
2024-06-22 21:23:47 -0400

    fix logic issues with delete and clear

--- src/script.js
+++ src/script.js
@@ -13,10 +13,11 @@ function divide(a, b) {
     return a / b;
 }
 
-let num1, globalOp, num2, displayValue;
+let num1, globalOp, num2, displayValue = '', waitForSecondNum = false;
 
 function operate(num1, op, num2) {
     let result;
+    let temp;
     switch (op) {
         case '+':
             result = add(num1, num2);
@@ -24,61 +25,80 @@ function operate(num1, op, num2) {
         case '-':
             result = subtract(num1, num2);
             break;
-        case '*':
+        case 'x':
             result = multiply(num1, num2);
             break;
         case '/':
             result = divide(num1, num2);
     }
-    return result;
+    return (Number.isInteger(result) ? result : result.toFixed(3) * 1);
 }
 
 
 const content = document.querySelector(".content");
 content.addEventListener("click", (event) => {
-    console.log(event.target);
     let btn = event.target.textContent;
-    console.log(btn);
     const numList = '0123456789.';
-    const opList = 'X/+-='
-    if (numList.includes(btn)) 
+    const opList = 'x/+-='
+    if (numList.includes(btn)) {
         handleNumber(btn);
+    }
 
-    else if (opList.includes(btn)) 
+    else if (opList.includes(btn))
         handleOp(btn);
-    
+
     else if (btn == 'clear') {
-        const display = document.querySelector(".display");
-        display.textContent = '0';
-    } 
-    
-    else if (btn == 'delete') {
-        const display = document.querySelector(".display");
-        display.textContent = '';
+
+        displayValue = '';
         num1 = 0;
         num2 = 0;
         globalOp = '';
     }
-    
+
+    else if (btn == 'delete') {
+        // remove last character if exists
+        if (displayValue) {
+            displayValue = String(displayValue)
+                .substring(0, displayValue.length - 1);
+        }
+    }
+    const display = document.querySelector(".display");
+    display.textContent = displayValue;
 });
 
 function handleNumber(num) {
     display = document.querySelector(".display");
-    display.textContent += num;
-    console.log('updating...');
+    // We need to replace display content when the first number is selected
+    // after clicking an op button
+    if (waitForSecondNum) {
+        displayValue = num;
+        waitForSecondNum = false;
+    } else {
+        displayValue += num;
+    }
 }
 
 function handleOp(op) {
     if (op === '=') {
         num2 = Number(displayValue);
-        let result = operate(num1, globalOp, num2)
-        displayValue = result;
-        num1 = result;
+        displayValue = operate(num1, globalOp, num2)
+        num1 = displayValue;
         globalOp = '';
+        waitForSecondNum = false;
     } else {
+        // There is already an operation queued.
+        // Compute this before adding next operation
+        if (globalOp) {
+            console.log('changing..')
+            console.log(num1, globalOp, Number(displayValue));
+            displayValue = operate(num1, globalOp, Number(displayValue))
+            console.log(displayValue);
+        }
         globalOp = op;
         num1 = Number(displayValue);
+        waitForSecondNum = true;
+
     }
-    
+
 
 }
